97b77573c36ce1db5b3710b55ddbd5797b29b6d9
Tosin5S <olyguy@gmail.com>
2023-02-03 23:18:37 +0100

    create a person object

---
+++ person.js
@@ -0,0 +1,17 @@
+const person = {
+  name: ["Bob", "Smith"],
+  age: 32,
+  bio: function () {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function () {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  },
+};
+
+person.name;
+person.name[0];
+person.age;
+person.bio();
+person.introduceSelf();
+
