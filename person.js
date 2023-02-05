d0e735b1e71e6024cd5d869ef24421c3d9c13910
Tosin5S <olyguy@gmail.com>
2023-02-05 22:28:34 +0100

    JavaScript object basics: shorter syntax and dot notation

--- person.js
+++ person.js
@@ -1,17 +1,17 @@
 const person = {
-  name: ["Bob", "Smith"],
-  age: 32,
-  bio: function () {
-    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  name: {
+    first: "Bob",
+    last: "Smith",
   },
-  introduceSelf: function () {
-    console.log(`Hi! I'm ${this.name[0]}.`);
+  age: 32,
+  bio () {
+    console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.`);
+  },
+  introduceSelf () {
+    console.log(`Hi! I'm ${this.name.first}.`);
   },
 };
 
-person.name;
-person.name[0];
-person.age;
 person.bio();
 person.introduceSelf();
 
